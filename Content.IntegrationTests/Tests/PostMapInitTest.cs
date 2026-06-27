a132d1200ec8d8a99f32578c1a0f8e70c746d37d
TiFeRi <tiferi14@gmail.com>
2026-06-27 20:48:41 +0300

    ERT

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -77,6 +77,7 @@ namespace Content.IntegrationTests.Tests
             "/Maps/_Fish/Station/centcomm.yml",
             "/Maps/_Fish/Shuttles/Antag/**",
             "/Maps/_Fish/Shuttles/DSO/**",
+            "/Maps/_Fish/Shuttles/DSO/ERT/**",
             "/Maps/_Fish/Nonstations/**",
             // Fish - End
         };
